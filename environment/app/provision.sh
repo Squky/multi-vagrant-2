5ab1584dfb314ad4bd3c1e6ef257e72e84b7a3c5
Squky <jarihussain@hotmail.co.uk>
2020-07-02 17:30:23 +0100

    sorted the reverse proxy stuff

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -14,12 +14,21 @@ sudo apt-get install git -y
 
 # install nodejs
 sudo apt-get install python-software-properties
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install nodejs -y
 
 # install pm2
 sudo npm install pm2 -g
 
 # npm install and start
-echo 'export DB_HOST=mongodb://192.168.10.200:27017/posts' >> ~/.bashrc
+#echo 'export DB_HOST=mongodb://192.168.10.200:27017/posts' >> ~/.bashrc
 sudo npm install
+
+
+sudo rm ../../etc/nginx/sites-available/default
+#sudo rm ../../etc/nginx/sites-available/reverse-proxy.conf
+
+sudo cp /home/ubuntu/reverse-prox/reverse-proxy.conf /etc/nginx/sites-available/
+
+sudo ln -s /etc/nginx/sites-available/reverse-proxy.conf /etc/nginx/sites-enabled/reverse-proxy.conf
+sudo systemctl restart nginx
